1756eef2f703880323a480b35daad3e147d57307
Sandeep Tanjore <xyz@gmail.com>
2025-09-14 21:54:23 -0400

    Started the loop challenege series

---
+++ section-1-javascript-bonus/10-loop-challenges.js
@@ -0,0 +1,22 @@
+/*
+* 1. Write a 'while' loop that calculates the sum of all numbers from 1 to 5
+* and stores the result in a variable named 'sum'
+* */
+
+let counter = 1,
+    sum = 0;
+
+while(counter <6){
+    sum = counter + sum;
+    //counter++;
+    counter = counter + 1;
+
+}
+
+console.log(`Sum of numbers from 1 to 5 is: ${sum}`);
+
+/*
+* 2.
+* **/
+
+
