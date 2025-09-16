dc95d6b8538ef7ea035f7b93270f11dda19fa2de
Sandeep Tanjore <sandeep.tanjore@mcgill.ca>
2025-09-16 15:05:20 -0400

    Progressing a few more loop challenges

--- section-1-javascript-bonus/11-ten-loop-challenges.js
+++ section-1-javascript-bonus/11-ten-loop-challenges.js
@@ -8,11 +8,25 @@ let allTeas = ['green tea', 'black tea', 'chai', 'oolang tea'],
   selectedTeas = [];
 
 for (let i = 0; i < allTeas.length; i++){
-
   if (allTeas[i] === 'chai') {
     break;
   }
   selectedTeas.push(allTeas[i]);
-};
+}
 
 console.log(selectedTeas);
+
+/**
+ * 2. Write a `for` loop that loops through the array
+ * `['London', 'New York', 'Paris', 'Berlin']` and skips 'Paris'.
+ * Store the other cities in a new array named `VisitedCities`
+ */
+
+let cities = ['London', 'New York', 'Paris', 'Berlin'],
+  VisitedCities = [];
+
+for (let i = 0; i < cities.length; i++){
+  do {
+     
+   }while(cities!=='Paris')
+}
